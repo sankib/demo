f88a04f0b056071eba9d30d7646974d68a400b96
Santos Ngeywa <35596478+sankib@users.noreply.github.com>
2021-03-11 07:50:20 +0300

    done

--- app/src/main/java/com/example/demo/ThirdFragment.java
+++ app/src/main/java/com/example/demo/ThirdFragment.java
@@ -14,14 +14,16 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+//Object declarations
 public class ThirdFragment extends Fragment {
     Button change_password_button;
     TextView conpasserror,passerror;
     TextInputEditText pass,passcon;
     @Override
+//Variable declarations
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_third, container, false);
+         View v = inflater.inflate(R.layout.fragment_third, container, false);
         pass = (TextInputEditText) v.findViewById(R.id.newPasswordText);
         change_password_button = (Button) v.findViewById(R.id.change_password_button);
         passcon = (TextInputEditText) v.findViewById(R.id.confirmPasswordText);
@@ -31,6 +33,19 @@ public class ThirdFragment extends Fragment {
 
 
     }
+//Error removal function
+//    private boolean removeError() {
+//        String passI = (String) pass.getText().toString();
+//        String passconI = (String) passcon.getText().toString().trim();
+//        if (!passI.isEmpty()) {
+//            passerror.setText("");
+//            return false;
+//        }
+//
+//        return false;
+//    }
+
+//Password validation
     private boolean validatePassword() {
         String passI = (String)pass.getText().toString();
         String passconI=(String) passcon.getText().toString().trim();
@@ -42,35 +57,47 @@ public class ThirdFragment extends Fragment {
             return false;
         }
         if (passI.equals(passconI)) {
-            conpasserror.setText("Password Matched");
-
+            conpasserror.setText("Passwords match");
+            NavHostFragment.findNavController(ThirdFragment.this)
+                    .navigate(R.id.action_ThirdFragment_to_FirstFragment);
             return false;
         }else {
-            conpasserror.setText("Password Would Not be matched");
+            conpasserror.setText("Passwords do not match");
             return true;
         }
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-
+    super.onViewCreated(view, savedInstanceState);
+//Button onClick listener for validation
         change_password_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     validatePassword();
-//                    if(validatePassword()==true){
-//                    NavHostFragment.findNavController(ThirdFragment.this)
-//                            .navigate(R.id.action_ThirdFragment_to_FirstFragment);}
+
+
                 }
 
             });
+
+
+// Removes error messages
+//        passerror.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                removeError();
+//
+//
+//            }
+//
+//        });
+
+
+        }
         }
 
 
-}
 
 
 
-//            pass.getText().toString().trim();
-//, TextInputEditText pass, TextInputEditText passcon, TextView passerror, TextView conpasserror
-//            passcon.getText().toString().trim(
+
